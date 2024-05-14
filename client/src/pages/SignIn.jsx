70311ad5eb34d9ca8b4edb96c39dc4a57eb73bd9
aman-sah <amankumarmdp88@gmail.com>
2024-05-14 12:21:36 +0530

    create and add OAuth conponent

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { signInStart, signInSuccess, signInfailure } from '../redux/user/userSlice'
+import OAuth from '../components/OAuth';
 export default function SignIn() {
   const [formData, setFormData] = useState({});
   const {loading, error } = useSelector((state) => state.user);
@@ -13,59 +14,59 @@ export default function SignIn() {
       [e.target.id]: e.target.value,
     });
   };
-  // const handleSubmit = async (e) => {
-  //   e.preventDefault();
-  //   try {
-  //     dispatch(signInStart());
-  //     const res = await fetch('/api/auth/signin',
-  //       {
-  //         method: 'POST',
-  //         headers: {
-  //           'Content-Type': 'application/json',
-  //         },
-  //         body: JSON.stringify(formData),
-  //       });
-  //     const data = await res.json();
-  //     console.log(data);
-  //     if (data.success == false) {
-  //       dispatch(signInfailure(data.message));
-  //       return;
-  //     }
-  //     dispatch(signInSuccess(data));
-  //     navigate('/');
-  //   } catch (error) {
-  //     dispatch(signInfailure(error.message));
-  //   }
-  // };
-
-
-
-
-
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
       dispatch(signInStart());
-      const res = await fetch('/api/auth/signin', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(formData),
-      });
+      const res = await fetch('/api/auth/signin',
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(formData),
+        });
       const data = await res.json();
       console.log(data);
-      if (data.success===false) {
+      if (data.success === false) {
         dispatch(signInfailure(data.message));
-        return; 
-      } 
+        return;
+      }
       dispatch(signInSuccess(data));
-      navigate('/')
-      
+      navigate('/');
     } catch (error) {
       dispatch(signInfailure(error.message));
     }
   };
+
+
+
+
+
+  // const handleSubmit = async (e) => {
+  //   e.preventDefault();
+  //   try {
+  //     dispatch(signInStart());
+  //     const res = await fetch('/api/auth/signin', {
+  //       method: 'POST',
+  //       headers: {
+  //         'Content-Type': 'application/json',
+  //       },
+  //       body: JSON.stringify(formData),
+  //     });
+  //     const data = await res.json();
+  //     console.log(data);
+  //     if (data.success===false) {
+  //       dispatch(signInfailure(data.message));
+  //       return; 
+  //     } 
+  //     dispatch(signInSuccess(data));
+  //     navigate('/')
+      
+  //   } catch (error) {
+  //     dispatch(signInfailure(error.message));
+  //   }
+  // };
 
 
   return (
@@ -75,6 +76,7 @@ export default function SignIn() {
         <input type="email" placeholder='email' className='border p-3 rounded-lg' id='email' onChange={handleChange} />
         <input type="password" placeholder='password' className='border p-3 rounded-lg' id='password' onChange={handleChange} />
         <button disabled={loading} className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>{loading ? 'Loading...' : 'Sign In'}</button>
+        <OAuth/>
       </form>
       <div className='flex gap-2 mt-5'>
         <p>Dont have an account?</p>
@@ -85,5 +87,5 @@ export default function SignIn() {
       {error && <p className='text-red-500 mt-5'>{error}</p>}
     </div>
 
-  )
+  );
 }
